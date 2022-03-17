7c6ba14c5d3b9984497f0fe65111215322ad8a9c
Mikołaj Badyl <mikolajbadyl@mailbox.org>
2022-03-17 09:03:08 +0100

    Form

--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    video_url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.video_url
--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-
+from .forms import VideoForm
 # Create your views here.
 def dashboard(request):
-    return HttpRequest('Kanapa')
+    if request.method == "POST":
+        pass
+    form = VideoForm()
+    return render(request, 'base.html', {'form': form})
