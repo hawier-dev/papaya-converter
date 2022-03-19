60f9fd175de1d3c3a17e3262de6c2d57dddf6a45
Mikołaj Badyl <mikolajbadyl@mailbox.org>
2022-03-19 19:50:38 +0100

    video finder

--- converter/views.py
+++ converter/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-from .forms import VideoForm
+from pytube import YouTube
+
 # Create your views here.
 def dashboard(request):
     if request.method == "POST":
-        pass
-    form = VideoForm()
-    return render(request, 'base.html', {'form': form})
+        link = request.POST['url']
+        video = YouTube(link)
+
+        # VIDEO DATA
+        title = video.title
+        author = video.author
+        thumbnail = video.thumbnail_url
+        quality = []
+        
+        # QUALITY
+        for stream in video.streams.order_by('resolution'):
+            if stream.resolution not in quality:
+                quality.append(stream.resolution)
+        quality.reverse()
+        return render(request, 'base.html', {'title': title, 'author': author, 'quality': quality, 'video_style': 'video-visible'})
+    return render(request, 'base.html', {'title': '', 'author': '','quality': [],'video_style': 'video-hidden'})
